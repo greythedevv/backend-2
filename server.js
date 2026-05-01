e018f7d984da57d0463d20062a057aca799100f2
Greatness <Oluwolegreatnessadeola@gmail.com>
2026-05-01 18:49:03 +0100

    feat(cors): install cors

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
 
+// middlewares
+app.use(cors())
+app.use(express.json())
+app.use(express.static("public"))
 const PORT = process.env.PORT || 3500
+
 
 // routes
 app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/public/index.html")
 })
-
-
-
-
 
 // app listening to the port
 app.listen(PORT, ()=> {
